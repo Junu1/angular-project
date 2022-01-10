e08067802d8436f3304ad1b943ee79cb9eb34357
Junu <junustha4@gmail.com>
2022-01-10 18:42:21 +0545

    first angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { bufferToggle, reduce } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'demoapp';
+  image = '/assets/1.jpg';
+  width = 100;
+  height = 100;
+  heading='Angular Project';
+  name='Junu';
+  sum=4;
+  customers:any= ['Anisha','Junu'];
+  color='blue';
+  
+  ngOnInit(): void {
+
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    GalleryComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule
